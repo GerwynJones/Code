9165f570b3d71c5605eb4b483aa11499995cf390
Gezerj <JonesG71@cardiff.ac.uk>
2016-10-04 23:04:18 +0100

    Euler method
    ODE using Euler method

---
+++ Euler ODE.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 23:23:53 2016
+
+@author: gezer
+"""
+
+from __future__ import division
+from numpy import polynomial
+import numpy as np
+from scipy.integrate import odeint
+
