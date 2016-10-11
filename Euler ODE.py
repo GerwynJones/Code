d7d8e2db6e2688d7b426e1667c358d82bd1e9038
Gerwyn Jones <JonesG71@cardiff.ac.uk>
2016-10-11 22:34:43 +0100

    Final version ODE
    literally just needs commenting and better formating of graphs

--- Euler ODE.py
+++ Euler ODE.py
@@ -4,17 +4,14 @@ Created on Tue Oct 04 09:37:15 2016
 
 @author: Gerwyn Jones
 """
-
 from __future__ import division
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
 def f(y, l):
     """ A function to call yprime and y """
     return y[1], l*y[0]
-
-
+    
 def euler(y, f, dt, l):
     """ The Y array has two values so we need to split them up 
     when using euler"""
@@ -26,47 +23,39 @@ def euler(y, f, dt, l):
     yp = yc + dt*f2  # yprime value
     
     return ya,yp
-           
-        
+               
 def ODEsolve(Tmax, N, f, method, ic): 
-          
-    dt = Tmax/N 
     
     t = np.zeros(N)   # defining the time array
-    t[0] = ic[0]
+    dt = Tmax/N; t[0] = ic[0]
 
     y = np.zeros((2,N))  # defining a y array containging the y values and yp values
-    y[0,0] = ic[1]
-    y[1,0] = ic[2]
+    y[0,0] = ic[1]; y[1,0] = ic[2]
     
-    for i in range(0,N-1):
+    for i in xrange(0,int(N)-1):
         y[:,i+1]  = method(y[:,i], f, dt , ic[3])
         t[i+1] = t[i] + dt
         
     return y, t 
     
 #lambda
-w = 2*pi; l = -w*w
+w = 2*np.pi; l = -w*w
 
-#initial times 
+#defining initial conditions 
 ti = 0; Tmax = 1
 yi = 1; ypi = 0
 
 #time steps 
-N = 1000
-n = np.array([N,2*N,4*N])
+N = 1500; n = np.array([N,2*N,4*N])
 
-#initial conditions
+#collecting initial conditions
 ic = np.array([ti, yi, ypi, l])  # initial time, final time, initial y and lambda
  
 # solving ODE
 R = [ODEsolve(Tmax, N, f, euler, ic) for i,N in enumerate(n)]
 
-def f1(t,l):
-    
-    y = np.cos(l*t)    
-    
-    return y # eq of d^2y/dt^2
+def f1(t,l): 
+    return np.cos(l*t)    # eq of d^2y/dt^2
 
 for i in range(len(R)):
     T = R[i][1]; Y = R[i][0][0]
@@ -84,23 +73,19 @@ for i in range(len(R)):
     plt.legend(loc='best')
     plt.grid() 
     
-def ConvergenceTest(Tmax, N, f, ic, method, order):
+def ConvergenceTest(Tmax, n, f, ic, method, order):
     
     R = [ODEsolve(Tmax, N, f, euler, ic) for i,N in enumerate(n)]  
-    Y1 = R[0][0][0]
-    Y2 = R[1][0][0]      
-    Y4 = R[2][0][0]
+    Y1 = R[0][0][0]; Y2 = R[1][0][0]; Y4 = R[2][0][0]
     
     diff1 = (Y1 - Y2[::2])
     diff2 = (2**order)*(Y2[::2] - Y4[::4])
     
     return diff1,diff2
 
-order = 1
+a,b = ConvergenceTest(Tmax, n, f, ic, euler, 1)
 
-a,b = ConvergenceTest(Tmax, n, f, ic, euler, order)
-
-figure()
+plt.figure()
 plt.subplot(2,1,1)
 plt.plot(a)
 plt.plot(b)
@@ -116,8 +101,19 @@ plt.ylabel(r'$Error$')
 plt.legend(loc='best')
 plt.grid() 
     
+dt = (1/2)**np.linspace(1,18,18); Na = Tmax/dt
 
+A = [ODEsolve(Tmax, n, f, euler, ic) for i, n in enumerate(Na)] 
 
+Ydt = [A[i][0][0][-1] for i in range(len(A))]
+Tdt = [A[i][1][-1] for i in range(len(A))]
 
-
-
+Te = np.array(Tdt); Ye = f1(Te,w)
+ 
+plt.figure()   
+plt.loglog(Na, Ydt-Ye)
+plt.xlabel(r'$Time$')
+plt.ylabel(r'$Y$') 
+plt.title('Graph of ODE')
+plt.legend(loc='best') 
+plt.grid()     
