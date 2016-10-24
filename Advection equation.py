4abe133c8416505c713c45eb9010d389aa46128a
Gerwyn Jones <JonesG71@cardiff.ac.uk>
2016-10-24 15:18:35 +0100

    Adv eq

---
+++ Advection equation.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 11:53:53 2016
+
+@author: Admin
+"""
+
+from __future__ import division
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+a = meshgrid(Nx,Nt)
+
+Nx = 
+Nt = 
+v = 
+
+n = Nx + 1
